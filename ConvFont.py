b0e3320bcfd93ca3707204cfe7af2e9e9c42749f
pockygom <johnjaeryungsong@msn.com>
2017-04-26 20:25:17 -0400

    Update ConvFont.py

--- ConvFont.py
+++ ConvFont.py
@@ -27,11 +27,11 @@ hidden_dim_1 = 16
 hidden_dim_2 = 32
 kernel = [9, 9]
 stride = 1
-reconst_dim_1 = input_dim_1-(kernel[0]-1)*4
-reconst_dim_2 = input_dim_2-(kernel[1]-1)*4
+reconst_dim_1 = input_dim_1-(kernel[0]-1)*2
+reconst_dim_2 = input_dim_2-(kernel[1]-1)*2
 reconst_dim = reconst_dim_1*reconst_dim_2*hidden_dim_2
 
-latent_dim = 100
+latent_dim = 128
 latent_stdev = 20
 fc_dim = latent_dim*10
 num_epochs = 50000
@@ -56,41 +56,41 @@ class Model():
         self.build_model()
     
     def Encoder(self, inputs):
-        with slim.arg_scope([slim.conv2d, slim.fully_connected],
+        with slim.arg_scope([slim.conv2d, slim.fully_connected], activation_fn=tf.nn.elu,
                             weights_initializer=tf.random_normal_initializer(stddev=0.01),
                             reuse=True):
             with slim.arg_scope([slim.conv2d], kernel_size=kernel, stride=stride, padding='VALID'):
                 output = slim.conv2d(inputs, hidden_dim_1, scope='enc1')
-                output = slim.conv2d(output, hidden_dim_2, scope='enc4')
+                output = slim.conv2d(output, hidden_dim_2, scope='enc2')
                 output = tf.reshape(output, [-1, reconst_dim*channels])
-                output = slim.fully_connected(output, fc_dim, scope='enc5')
-                output = slim.fully_connected(output, latent_dim, activation_fn=None, scope='enc6')
+                output = slim.fully_connected(output, fc_dim, scope='enc3')
+                output = slim.fully_connected(output, latent_dim, activation_fn=None, scope='enc4')
         return output
     
     def Decoder(self, inputs, labels):
-        with slim.arg_scope([slim.convolution2d_transpose, slim.fully_connected],
+        with slim.arg_scope([slim.convolution2d_transpose, slim.fully_connected], activation_fn=tf.nn.elu,
                             weights_initializer=tf.random_normal_initializer(stddev=0.01),
                             reuse=True):
             with slim.arg_scope([slim.convolution2d_transpose], kernel_size=kernel, stride=stride, padding='VALID'):
                 output = slim.fully_connected(inputs, fc_dim, scope='dec1') + slim.fully_connected(labels, fc_dim, scope='dec2')
                 output = slim.fully_connected(output, reconst_dim, scope='dec3')
                 output = tf.reshape(output, [-1, reconst_dim_1, reconst_dim_2, hidden_dim_2])
-                output = slim.convolution2d_transpose(output, hidden_dim_1, scope='dec6')
-                output = slim.convolution2d_transpose(output, channels, scope='dec7')
+                output = slim.convolution2d_transpose(output, hidden_dim_1, scope='dec4')
+                output = slim.convolution2d_transpose(output, channels, activation_fn=tf.nn.sigmoid, scope='dec5')
         return output
     
     def Discriminator(self, inputs):
-        with slim.arg_scope([slim.convolution2d_transpose, slim.fully_connected],
+        with slim.arg_scope([slim.convolution2d_transpose, slim.fully_connected], activation_fn=tf.nn.elu,
                             weights_initializer=tf.random_normal_initializer(stddev=0.01),
                             reuse=True):
             with slim.arg_scope([slim.convolution2d_transpose], kernel_size=kernel, stride=stride, padding='VALID'):
                 output = slim.fully_connected(inputs, fc_dim, scope='disc1')
                 output = slim.fully_connected(output, reconst_dim, scope='disc2')
                 output = tf.reshape(output, [-1, reconst_dim_1, reconst_dim_2, hidden_dim_2])
-                output = slim.convolution2d_transpose(output, hidden_dim_1, scope='disc5')
-                output = slim.convolution2d_transpose(output, channels, scope='disc6')
+                output = slim.convolution2d_transpose(output, hidden_dim_1, scope='disc3')
+                output = slim.convolution2d_transpose(output, channels, scope='disc4')
                 output = tf.reshape(output, [-1, input_dim*channels])
-                output = slim.fully_connected(output, 1, activation_fn=None, scope='disc7')
+                output = slim.fully_connected(output, 1, activation_fn=None, scope='disc5')
         return output
     
     def build_model(self):
